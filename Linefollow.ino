a9462043e4864a735c88483a55d8419a0af1c94e
Sondre Kaldhol Ulleland <167238379+GamingDragon006@users.noreply.github.com>
2025-09-08 10:59:46 +0200

    Update Linefollow.ino

--- Linefollow.ino
+++ Linefollow.ino
@@ -2,7 +2,7 @@
 
 // --- Sensor setup ---
 #define NUM_SENSORS 6
-//ZumoReflectanceSensorArray reflectanceSensors;
+ZumoReflectanceSensorArray reflectanceSensors;
 unsigned int sensorValues[NUM_SENSORS];
 
 // --- SparkFun TB6612FNG motor driver pins ---
